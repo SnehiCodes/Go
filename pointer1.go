694e80cedd4b90efcea806d35fe083df2b57c10a
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-12-06 23:23:58 +0530

    Add files via upload

---
+++ pointer1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func changeValue(str *string) {
+	*str = "changed"
+}
+func changeValue2(str string) {
+	str = "changed"
+}
+func main() {
+	toChange := "hello"
+	fmt.Println(toChange)
+	changeValue(&toChange)
+	fmt.Println(toChange)
+}
