18c8b362483fef72982d1127aafcf7a8aaeee8d5
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-12-08 00:19:56 +0530

    also about PANIC

---
+++ panic2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("start")
+	panicker()
+	fmt.Println("end")
+}
+func panicker() {
+	fmt.Println("about to panic")
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("ERROR", err)
+		}
+	}()
+	panic("something bad happened")
+	fmt.Println("done panicking")
+}
